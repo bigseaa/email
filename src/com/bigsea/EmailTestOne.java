b6678a4d65a00b69945a0d910023270556ed94f2
bigseaa <506798278@qq.com>
2019-05-26 11:24:22 +0800

    发送邮件

--- src/com/bigsea/EmailTestOne.java
+++ src/com/bigsea/EmailTestOne.java
@@ -1,13 +1,18 @@
 package com.bigsea;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.*;
 import java.util.Properties;
 
+/**
+ * 读取html文件，并以此为模板作为发送邮件的内容
+ * @Author yuhai
+ * @Date 2019-05-26
+ */
 public class EmailTestOne {
     public static void main(String[] args) throws MessagingException {
         String fromEmail = "****@qq.com";
@@ -16,12 +21,22 @@ public class EmailTestOne {
         String password = "****";
 
         String title = "测试邮件标题";
-        String content = "测试内容";
         EmailTestOne emailTestOne = new EmailTestOne();
+        String content = emailTestOne.readTemplate();
+        content = content.replace("${time}", "2012-12-12");
         emailTestOne.sendMail(fromEmail, toEmails, username, password,title, content);
-
     }
 
+    /**
+     * 发送邮件
+     * @param fromEmail 发件人
+     * @param toEmails 收件人
+     * @param username 发件人用户名
+     * @param password 密码
+     * @param title 标题
+     * @param content 内容
+     * @throws MessagingException
+     */
     public void sendMail(String fromEmail, String[] toEmails, String username, String password,
                          String title, String content) throws MessagingException {
         Properties properties = new Properties();
@@ -47,7 +62,11 @@ public class EmailTestOne {
         // 设置邮件标题
         message.setSubject(title);
         // 设置邮件内容
-        message.setText(content);
+        Multipart multipart = new MimeMultipart();
+        BodyPart bodyPart = new MimeBodyPart();
+        bodyPart.setContent(content, "text/html;charset=utf-8");
+        multipart.addBodyPart(bodyPart);
+        message.setContent(multipart);
         // 得到邮差对象
         Transport transport = session.getTransport();
         // 连接自己的邮箱账户
@@ -55,5 +74,40 @@ public class EmailTestOne {
         // 发送邮件
         transport.sendMessage(message, message.getAllRecipients());
         transport.close();
+    }
+
+    /**
+     * 读取模板文件
+     * @return 模板文件字符串
+     */
+    public String readTemplate() {
+        StringBuffer sb = new StringBuffer();
+        InputStreamReader in = null;
+        BufferedReader br = null;
+        // 加载模板
+        String tamplatePath = this.getClass().getClassLoader().getResource("").getPath();
+        tamplatePath = tamplatePath + "/com/bigsea/email_temp.html";
+        // 读取模板中的数据
+        File file = new File(tamplatePath);
+        try {
+            in = new InputStreamReader(new FileInputStream(file));
+            br = new BufferedReader(in);
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 }
